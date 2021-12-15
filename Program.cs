4b1a48bada319525f0171cd519957397531c48ce
Administrator <Administrator@DESKTOP-LO5FOLT>
2021-12-15 10:31:36 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine("Ömer DOĞU");
+
+using System.Collections;
+
+var sehirler = new Hashtable(); //tanımlma yaptık.  Hashtable bizim kontratlara uymak zozundayız. 
+//ekleme yapmaya geldik. 
+sehirler.Add(6, "Ankara"); // hash fonksiyonu var ve 6 elemanı olarak ekledik. 
+sehirler.Add(34, "İstanbul");
+sehirler.Add(55, "Samsun");
+sehirler.Add(38, "Kayseri");//bellekte nasıl görüntüsüvar bakalaım. 
+
+Console.ReadLine();
